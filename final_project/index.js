30fd309087dda2e90991d38185a3f3e527a38bf4
CesarSuarezWD <cesarsuarezmedina@gmail.com>
2024-10-10 18:38:43 -0500

    [FEATURE] General functions were implemented

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({message: 'No token provided'});
+    }
+
+    jwt.verify(token, 'estoy_logueado', (err, decoded) => {
+        if (err) {
+            return res.status(403).json({message: 'Failed to authenticate token'});
+        }
+
+        req.session.userId = decoded.id;
+        next();
+    })
 });
  
 const PORT =5000;
